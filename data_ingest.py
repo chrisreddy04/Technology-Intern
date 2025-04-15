4ce1f10b1092c60f3263821f779a11044831d1cc
chrisreddy04 <christurajreddy@gmail.com>
2025-04-15 18:45:58 -0400

    NumPy datetime error fix on data_ingest.py

--- data_ingest.py
+++ data_ingest.py
@@ -1,5 +1,6 @@
 # data_ingest.py
 import os
+import numpy as np
 import pandas as pd
 import psycopg2
 from psycopg2.extras import execute_values
@@ -45,7 +46,17 @@ def transform_data(df):
     return df
 
 def insert_data(df):
-   
+
+    df = df.astype({
+        'ai_adoption_rate': float,
+        'ai_content_volume_tb': float,
+        'job_loss_due_to_ai': float,
+        'revenue_increase_due_to_ai': float,
+        'human_ai_collab_rate': float,
+        'consumer_trust_in_ai': float,
+        'market_share_ai_companies': float
+    })
+
     conn = psycopg2.connect(
         host=os.environ.get("DATABASE_HOST", "localhost"),
         dbname=os.environ.get("DATABASE_NAME", "mydatabase"),
@@ -54,13 +65,28 @@ def insert_data(df):
     )
     cursor = conn.cursor()
     
-    # Prepare tuples from the DataFrame
-    data_tuples = list(df[['country', 'published_date', 'industry', 'ai_adoption_rate',
-                             'ai_content_volume_tb', 'job_loss_due_to_ai',
-                             'revenue_increase_due_to_ai', 'human_ai_collab_rate',
-                             'top_ai_tools_used', 'regulation_status',
-                             'consumer_trust_in_ai', 'market_share_ai_companies'
-                            ]].to_records(index=False))
+
+    raw_data_tuples = df[[
+        'country', 'published_date', 'industry', 'ai_adoption_rate',
+        'ai_content_volume_tb', 'job_loss_due_to_ai', 'revenue_increase_due_to_ai',
+        'human_ai_collab_rate', 'top_ai_tools_used', 'regulation_status',
+        'consumer_trust_in_ai', 'market_share_ai_companies'
+    ]].to_records(index=False)
+
+    data_tuples = []
+    for record in raw_data_tuples:
+        new_record = []
+        for item in record:
+            # If item is a NumPy datetime, convert using pandas Timestamp
+            if isinstance(item, np.datetime64):
+                new_item = pd.Timestamp(item).to_pydatetime()
+            # If item is a NumPy generic numeric type, convert it to a native Python float/int using .item()
+            elif isinstance(item, np.generic):
+                new_item = item.item()
+            else:
+                new_item = item
+            new_record.append(new_item)
+        data_tuples.append(tuple(new_record))
    
     insert_query = """
         INSERT INTO ai_content_impact (
